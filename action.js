75f962df73b9649d277df19254466b4fd29b8af1
Rodnaf34 <60832022+Rodnaf34@users.noreply.github.com>
2022-03-26 19:59:00 +0800

    testing a button dev

--- action.js
+++ action.js
@@ -1,2 +1,6 @@
-window.alert('Hello World!');
+const hello = window.alert('Hello World!');
+
+let buttonTarget = document.getElementById('button');
+
+buttonTarget.addEventListener('click', hello);
 
